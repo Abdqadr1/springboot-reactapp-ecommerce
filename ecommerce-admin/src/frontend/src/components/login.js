939f32be265960ce2a8a9c6558408a01ee4744d0
Abdqadr1 <abolarinwaquadri@gmail.com>
2022-07-20 17:06:58 +0800

    admin login page

--- ecommerce-admin/src/frontend/src/components/login.js
+++ ecommerce-admin/src/frontend/src/components/login.js
@@ -5,11 +5,10 @@ import { useNavigate, useParams } from 'react-router-dom';
 import "../css/login.css";
 import useAuth from './custom_hooks/use-auth';
 import { getFormData, SPINNERS_BORDER_HTML } from './utilities';
-import useSettings from './custom_hooks/use-settings';
+import login from "../images/login.png";
 
 const Login = () => {
     const loginUrl = process.env.REACT_APP_SERVER_URL+"login";
-    const { SITE_LOGO } = useSettings();
     const alertRef = useRef();
     const buttonRef = useRef();
     const navigate = useNavigate();
@@ -60,30 +59,35 @@ const Login = () => {
         if(type === 2) setParam({ show: ' text-warning', message: "You've been logged out" })
     }, [out])
 
-    return ( 
-        <Row className="login-body p-3 justify-content-center mx-0">
-            <Col xs={12} md={8} lg={6} className="h-fit-content my-auto">
-                <img className='login-logo' alt="logo" src={SITE_LOGO} />
-                <h4 className={`text-center py-3` + param.show}>{param.message}</h4>
-                <Form className="border pt-2 pb-5 px-3 rounded" onSubmit={handleSubmit}>
-                    <h5 className={`text-center py-3`}>Access to QShop Control Panel</h5>
-                    <Alert ref={alertRef} className="text-center" tabIndex={-1} variant="danger" show={alert.show} dismissible onClose={toggleAlert}>
-                        {alert.msg}
-                    </Alert>
-                    <Form.Group className="mb-3" controlId="email">
-                        <Form.Label className="mb-2">Email address:</Form.Label>
-                        <Form.Control value={form.email} onInput={handleInput} required type="email" placeholder="Enter email address" />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="password">
-                        <Form.Label className="mb-2">Password:</Form.Label>
-                        <Form.Control value={form.password} onInput={handleInput} required type="password" />
-                    </Form.Group>
-                    <Button ref={buttonRef} variant="primary" type="submit" className="mx-auto d-block">
-                        Log In
-                    </Button>
-                </Form>
-            </Col>
-        </Row>
+    return (
+        <>
+            
+            <Row className="mx-0 px-md-5 justify-content-center align-items-center bg-light" style={{'height': '100vh'}}>
+                <Col md="6" className="p-0 d-none d-sm-none d-md-flex">
+                    {/* <img style={{width: '100%', height: '100%'}} src={login} alt="login" /> */}
+                </Col>
+                <Col md="6" sm={12} className="text-center px-auto" style={{'height': '80%'}}>
+                    <Form className="mx-auto shadow rounded px-5 bg-light py-md-5" onSubmit={handleSubmit} style={{'height': '100%'}}>
+                        <h1 className="mt-2">ADMIN LOGIN</h1>
+                        <h4 className={`text-center py-3` + param.show}>{param.message}</h4>
+                        <Alert ref={alertRef} className="text-center" tabIndex={-1} variant="danger" show={alert.show}
+                            dismissible onClose={toggleAlert}>
+                            {alert.msg}
+                        </Alert>
+                        <Form.Group className="my-3" controlId="email">
+                            <Form.Label className="text-start w-100 fw-bold">Email:</Form.Label>
+                            <Form.Control value={form.email} onInput={handleInput} className="input bg-light" name="email" type="email" placeholder="Enter email" required maxLength="64"/>
+                        </Form.Group>
+                        <Form.Group className="mt-3 mb-4" controlId="password">
+                            <Form.Label className="text-start w-100 fw-bold">Password:</Form.Label>
+                            <Form.Control value={form.password} onInput={handleInput} className="input bg-light" name="password" type="password" placeholder="Enter password" minLength="5" required/>
+                        </Form.Group>
+                        <Button ref={buttonRef} variant="primary" className="mt-3 mb-2 py-3 btn-gradient" style={{ width: "100%" }}
+                            type="submit">Log in</Button>
+                    </Form>
+                </Col>
+            </Row>
+        </> 
      );
 }
  
